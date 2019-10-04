39ce8d5e18a49d4709ad8d3c08baf3a586ae6c50
kainhuck <35602940+kainhuck@users.noreply.github.com>
2019-10-04 10:42:01 +0800

    views

---
+++ views/index.py
@@ -0,0 +1,22 @@
+import tornado.web
+from tornado.web import RequestHandler, StaticFileHandler
+
+# 上传文件使用
+import config
+import os
+
+# 数据库使用
+# from models import ClassName
+
+# 定义自己的StaticFileHandler
+class StaticFileHandler(StaticFileHandler):
+	def __init__(self, *args, **kwargs):
+
+		super(StaticFileHandler, self).__init__(*args, **kwargs)
+		self.xsrf_token
+
+'''
+# 在这里创建你的Handlers
+class YourHandler(RequestHandler):
+	pass
+'''
