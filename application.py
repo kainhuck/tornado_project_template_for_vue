9a323533f4f0cfe352b3876f631c54dd3a4f4d8d
kainhuck <35602940+kainhuck@users.noreply.github.com>
2019-10-06 15:42:50 +0800

    Update application.py

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ class Application(tornado.web.Application):
         handlers = [
 
             # StaticFileHandler,注意要放在所有路由的最下面
-            (r'/(.*)$', index.StaticFileHandler,
+            (r'/(.*)$', index.StaticFileHandler_,
              {"path": os.path.join(config.BASE_DIRS, 'static/html'), "default_filename": "index.html"})
         ]
         super(Application, self).__init__(handlers, **config.settings)
